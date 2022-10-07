946f1479467b7d83eac0f239051630ecc4f4e93b
Pedro Nogueira <pedromdsn.2002@hotmail.com>
2022-10-07 03:06:19 +0100

    fix: :bug: html lang

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,7 +14,7 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang="pt">
         <Head>
           <link rel="icon" href="favicon.ico" type="image/x-icon" />
           <meta
